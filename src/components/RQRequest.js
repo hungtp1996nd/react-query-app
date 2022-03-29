49049749d1f4129bdbce2435186ce51e40d6380e
Truong Phuc Hung <hungtp@vnext.com.vn>
2022-03-29 10:35:00 +0700

    fetching data by onClick with react-query

--- src/components/RQRequest.js
+++ src/components/RQRequest.js
@@ -7,15 +7,16 @@ const fetchSuperHeroes = () => {
 }
 
 const RQRequest = () => {
-  const { isLoading, data, error, isError } = useQuery('super-heroes', fetchSuperHeroes)
+  const { isLoading, data, error, isError, isFetching, refetch } = useQuery('super-heroes', fetchSuperHeroes, { enabled: false })
 
-  if (isLoading) return <h2>Loading...</h2>
+  if (isLoading || isFetching ) return <h2>Loading...</h2>
 
   if (isError) return <h2>{error.message}</h2>
 
   return (
     <div>
       <h2>React Query Request</h2>
+      <button onClick={refetch}>Fetch heroes</button>
       {
         data?.data.map((hero) => {
           return <div key={hero.name}>{hero.name}</div>
