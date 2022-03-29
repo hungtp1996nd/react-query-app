b20682912159fe4333dc51157e7deea866efed17
Truong Phuc Hung <truong_phuc_hung@yahoo.com.vn>
2022-03-29 22:59:28 +0700

    initial data (react-query) - caching data from list when showing detail

--- src/hooks/useSuperHeroDetailData.js
+++ src/hooks/useSuperHeroDetailData.js
@@ -1,4 +1,4 @@
-import {useQuery} from "react-query";
+import { useQuery, useQueryClient } from "react-query";
 import axios from 'axios'
 
 const fetchHeroDetail = (heroId) => {
@@ -6,5 +6,21 @@ const fetchHeroDetail = (heroId) => {
 }
 
 export const useSuperHeroDetailData = (heroId) => {
-  return useQuery(['hero-detail', heroId], () => fetchHeroDetail(heroId))
+  const queryClient = useQueryClient();
+  return useQuery(
+    ['hero-detail', heroId],
+    () => fetchHeroDetail(heroId),
+    {
+      // when chosen "slow 3G" at network tab, click into detail hero --> Loading progress occur
+      // initial data make quick action when showing detail by caching with Javascript logic --> not using query
+      initialData: () => {
+        const hero = queryClient.getQueryData('super-heroes')?.data?.find(hero => hero.id === parseInt(heroId))
+        if (hero) {
+          return { data: hero }
+        } else {
+          return undefined;
+        }
+      }
+    }
+  )
 }
